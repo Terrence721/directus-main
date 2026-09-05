4a61dae21f5ff3ebcd96bb6dc0f5f411baa5642a
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-09-05 04:49:36 -0400

    Export isDirectusError and ForbiddenError from packages/errors

--- packages/errors/src/index.ts
+++ packages/errors/src/index.ts
@@ -1,1 +1,4 @@
 export { DirectusError } from './create-error.js';
+export { isDirectusError } from './is-directus-error.js';
+export { ForbiddenError } from './errors/forbidden.js';
+export type { ForbiddenErrorExtensions } from './errors/forbidden.js';
