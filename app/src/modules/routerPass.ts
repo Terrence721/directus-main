60225c7ce46cc3858f12558a6262d71909c0f0bf
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-09-19 17:40:16 -0400

    Add RouterPass: passthrough component for nested module routes

---
+++ app/src/modules/routerPass.ts
@@ -0,0 +1,8 @@
+import { h, type Component } from 'vue';
+import { RouterView } from 'vue-router';
+
+const RouterPass: Component = () => h(RouterView);
+
+RouterPass.displayName = 'RouterPass';
+
+export default RouterPass;
